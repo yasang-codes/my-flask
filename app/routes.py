87441d2edc5152d6bfe1404e5ff19d260c952cf1
yangara <yangara@codeblock.co.in>
2024-08-19 13:06:49 +0530

    feat: :alembic: experimenting with css and an index page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/login/submit', methods=['POST'])
+def login_submit():
+    username = request.form['username']
+    password = request.form['password']
+
+    return "Username: " + username + " Password: " + password
+    

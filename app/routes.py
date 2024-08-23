1b4c47d87ac9537fda272197ef72eeba7dbad8d6
yangara <yangara@codeblock.co.in>
2024-08-23 10:34:55 +0530

    feat: :sparkles: now uses flask login for session management

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,28 @@
 from app import app
 import secrets
-from flask import render_template, request, redirect, session, flash
+from flask import render_template, request, redirect, flash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from app.classes.user import USERS, Anonymous, USER_NAMES
 
 app.secret_key = secrets.token_hex(16)
 
+login_manager = LoginManager()
+
+login_manager.anonymous_user = Anonymous
+
+@login_manager.user_loader
+def load_user(id):
+    return USERS.get(int(id))
+
+login_manager.setup_app(app)
+
 @app.route('/')
 def index():    
-    if 'username' not in session:
-        return redirect('/login')
-    else:
+    if current_user.is_authenticated:
         return render_template('index.html')
+    else:
+        return redirect('/login')
+        
 
 @app.route('/login')
 def login():
@@ -19,12 +32,16 @@ def login():
 def login_submit():
     username = request.form['username']
     password = request.form['password']
-
-    if (username == 'admin' and password == 'Password123'):
-        session['username'] = username
-        flash(username)
-        return redirect('/')
-    elif (username == 'admin'):
+    remember = request.form.get('remember')
+    
+    if (username in USER_NAMES and password == 'Password123'):
+        if(login_user(USER_NAMES[username], remember == 'on')):            
+            flash(username)
+            return redirect('/')
+        else:
+            flash('Login failed. Please contact the administrator.')
+            return redirect('/login')
+    elif (username in USER_NAMES and password != 'Password123'):
         flash('Username and password do not match. Please try again.')
         return redirect('/login')
     else:
@@ -32,8 +49,10 @@ def login_submit():
         return redirect('/login')
     
 @app.route('/logout', methods=['POST'])
+@login_required
 def logout():
-    session.pop('username', None)
+    logout_user()
+    flash('You have been logged out.')
     return redirect('/')    
 
         
